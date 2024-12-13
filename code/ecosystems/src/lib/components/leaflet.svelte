564c028be1dfef8033a3f7a86a8a452865479db5
Erpix3lt <maximilian.schmalenbach@hotmail.com>
2024-12-13 21:16:50 +0100

    use https

--- code/ecosystems/src/lib/components/leaflet.svelte
+++ code/ecosystems/src/lib/components/leaflet.svelte
@@ -36,7 +36,7 @@
     }
 
     map = L.map(mapElement!);
-    L.tileLayer("http://{s}.google.com/vt/lyrs=s&x={x}&y={y}&z={z}", {
+    L.tileLayer("https://{s}.google.com/vt/lyrs=s&x={x}&y={y}&z={z}", {
       subdomains: ["mt0", "mt1", "mt2", "mt3"],
     }).addTo(map);
 
