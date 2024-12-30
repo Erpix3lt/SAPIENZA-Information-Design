c00e0a468817211bc83d67afce2b879a1cc0ff56
Erpix3lt <maximilian.schmalenbach@hotmail.com>
2024-12-30 14:12:58 +0100

    init next app

--- code/ecosystems/src/lib/components/header.svelte
+++
@@ -1,9 +0,0 @@
-<div class="grid grid-cols-5 place-items-end items-start mt-2 font-arialBlack text-cabbage-pont-400 text-xl uppercase">
-  <a href="/explore">Explore</a>
-  <a href="https://osv.dev">Dataset</a>
-  <div class="flex flex-col place-items-end">
-    <a href="https://github.com">Github</a>
-    <a href="https://openforumeurope.org/open-source-impact-study/">References</a>
-  </div>
-  <a href="#" >Poster</a>
-</div>
