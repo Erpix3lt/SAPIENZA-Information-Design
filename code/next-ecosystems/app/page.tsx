04b9e5606e898f4bcdbad85d7213378d22c4d92a
Erpix3lt <maximilian.schmalenbach@hotmail.com>
2024-12-30 16:15:07 +0100

    info on hover

--- code/next-ecosystems/app/page.tsx
+++ code/next-ecosystems/app/page.tsx
@@ -1,21 +1,40 @@
 'use client'
 
 import { ThreeScene } from "@/components/three-scene";
+import { useState } from "react";
 
 export default function Home() {
+  const [displayBlur, setDisplayBlur] = useState(true);
+  const [isBlurred, setIsBlurred] = useState(true);
+
+  const handleDisplayBlur = () => {
+    setIsBlurred(false);
+    setDisplayBlur(false);
+  }
+
+  const handleNoBlur = () => {
+    setIsBlurred(false);
+  };
+
+  const handleBlur = () => {
+    if(displayBlur){
+      setIsBlurred(true);
+    }
+  };
+  
   return (
     <div className="h-screen w-full flex flex-col justify-between">
-      <div className="blur-xl">
-      <ThreeScene></ThreeScene>
-
+      <div className={`transition-all duration-500 ${isBlurred ? 'blur-xl' : ''}`}>
+        <ThreeScene onClick={handleDisplayBlur} onHover={handleNoBlur} onNoHover={handleBlur}></ThreeScene>
       </div>
       <footer className="text-white text-xs my-2 mx-1">
         <div className="grid grid-cols-5 gap-4">
           <div className="flex flex-col justify-end">
             <p>© 2024</p>
             <p>Open-Source vulnerabilities</p>
-            <br />
+            <p>&nbsp;</p>
             <p>A data visualisation project</p>
+            <p><a href="/poster">Poster</a></p>
           </div>
           <div className="flex flex-col justify-end">
             <p>23000</p>
@@ -24,16 +43,16 @@ export default function Home() {
             <p>Environments</p>
           </div>
           <div className="col-span-2 flex flex-col justify-end">
+            <p className={`border w-fit px-1 border-pink-500 transition-all duration-500 ${isBlurred ? 'blur-xl' : ''}`}>Click object to enter scene</p>
             <p>i:</p>
             <p>This project showcases vulnerabilities in different opensource ecosystems. In order to explore simply choose a ecosystem and interact.</p>
-            <br />
+            <p>&nbsp;</p>
             <p>All data is sourced from <a href="https://osv.dev">osv.dev</a></p>
           </div>
           <div className="flex flex-col justify-end">
             <p>Open source software has a beneficial impact on the EU. Therefore, increased funding is needed. Our data visualisation projects aim at creating awareness.</p>
-            <br />
+            <p>&nbsp;</p>
             <p>Follow the EU´s open source report for more details</p>
-            <p><a href="/poster">Poster</a></p>
           </div>
         </div>
       </footer>
--- code/next-ecosystems/components/three-scene.tsx
+++ code/next-ecosystems/components/three-scene.tsx
@@ -1,20 +1,49 @@
-import React from 'react';
-import { Canvas } from '@react-three/fiber';
-import { OrbitControls } from '@react-three/drei';
+import React, { useRef } from "react";
+import { Canvas, useFrame } from "@react-three/fiber";
+import * as THREE from 'three';
+import { OrbitControls } from "@react-three/drei";
 
-export const ThreeScene = () => {
+interface ThreeSceneProps {
+  onClick: () => void;
+  onHover: () => void;
+  onNoHover: () => void;
+}
+
+const RotatingMesh: React.FC<ThreeSceneProps> = ({ onClick, onHover, onNoHover }) => {
+  const meshRef = useRef<THREE.Mesh>(null);
+
+  useFrame(() => {
+    if (meshRef.current) {
+      meshRef.current.rotation.y += 0.005;
+    }
+  });
+
   return (
-    <div style={{ height: '80vh', width: '100vw' }}>
- <Canvas>
-      <ambientLight />
-      <pointLight position={[10, 10, 10]} />
-      <mesh>
+    <mesh
+      ref={meshRef}
+      onClick={onClick}
+      onPointerOver={onHover}
+      onPointerLeave={onNoHover}
+    >
       <boxGeometry args={[1, 1, 1]} />
       <meshStandardMaterial color="orange" />
     </mesh>
-      <OrbitControls />
-    </Canvas>
+  );
+};
+
+export const ThreeScene: React.FC<ThreeSceneProps> = ({
+  onClick,
+  onHover,
+  onNoHover,
+}) => {
+  return (
+    <div style={{ height: "80vh", width: "100vw" }}>
+      <Canvas>
+        <ambientLight />
+        <pointLight position={[10, 10, 10]} />
+        <RotatingMesh onClick={onClick} onHover={onHover} onNoHover={onNoHover} />
+        <OrbitControls />
+      </Canvas>
     </div>
-   
   );
 };
