204ae39fb60b68baacf6d75e87d52baf3e671b12
Erpix3lt <maximilian.schmalenbach@hotmail.com>
2024-12-31 14:21:20 +0100

    interactivity, swithc between ecosystems

--- code/next-ecosystems/app/page.tsx
+++ code/next-ecosystems/app/page.tsx
@@ -3,29 +3,66 @@
 import { ThreeScene } from "@/components/three-scene";
 import { useState } from "react";
 
+export type Ecosystem = { 
+  ecosystem: string,
+  gltfPath: string,
+  color: string,
+ };
+const ecosystems: Ecosystem[] = [
+  { ecosystem: 'npm', gltfPath: '/path/to/npm.gltf', color: 'red' },
+  { ecosystem: 'maven', gltfPath: '/path/to/maven.gltf', color: 'green' },
+  { ecosystem: 'pypi', gltfPath: '/path/to/pypi.gltf', color: 'blue' },
+  { ecosystem: 'rubygems', gltfPath: '/path/to/rubygems.gltf', color: 'yellow' },
+  { ecosystem: 'debian', gltfPath: '/path/to/debian.gltf', color: 'cyan' },
+  { ecosystem: 'github', gltfPath: '/path/to/github.gltf', color: 'magenta' }
+];
+
 export default function Home() {
   const [displayBlur, setDisplayBlur] = useState(true);
   const [isBlurred, setIsBlurred] = useState(true);
+  const [ecosystem, setEcosystem] = useState<Ecosystem>(ecosystems[0]);
 
-  const handleDisplayBlur = () => {
+  const handleObjectClick = () => {
     setIsBlurred(false);
     setDisplayBlur(false);
   }
 
-  const handleNoBlur = () => {
+  const handleObjectEnter = () => {
     setIsBlurred(false);
   };
 
-  const handleBlur = () => {
+  const handleObjectLeave = () => {
     if(displayBlur){
       setIsBlurred(true);
     }
   };
+
+  const handleDecrementEcosystem = () => {
+    setEcosystem((prev) => {
+      const currentIndex = ecosystems.indexOf(prev);
+      const newIndex = currentIndex === 0 ? ecosystems.length - 1 : currentIndex - 1;
+      return ecosystems[newIndex];
+    });
+  }
+  const handleIncrementEcosystem = () => {
+    setEcosystem((prev) => {
+      const currentIndex = ecosystems.indexOf(prev);
+      const newIndex = currentIndex === ecosystems.length - 1 ? 0 : currentIndex + 1;
+      return ecosystems[newIndex];
+    });
+  }
+
   
   return (
     <div className="h-screen w-full flex flex-col justify-between">
       <div className={`transition-all duration-500 ${isBlurred ? 'blur-xl' : ''}`}>
-        <ThreeScene onClick={handleDisplayBlur} onHover={handleNoBlur} onNoHover={handleBlur}></ThreeScene>
+        <ThreeScene ecosystem={ecosystem} onClick={handleObjectClick} onHover={handleObjectEnter} onNoHover={handleObjectLeave}></ThreeScene>
+      </div>
+      <div>
+      <div className="text-white grid grid-cols-5 gap-4 mx-1 mb-2">
+        <button className={`col-start-2 w-fit transition-all duration-500 leading-none ${isBlurred ? '' : 'blur-xl'}`} onClick={handleDecrementEcosystem}>{"<"}</button>
+        <p className={`text-xs w-fit ${isBlurred ? '' : 'underline'}`}>Click object to enter scene</p>
+        <button className={`col-start-5 w-fit h-fit transition-all duration-500 leading-none ${isBlurred ? '' : 'blur-xl'}`} onClick={handleIncrementEcosystem}>{">"}</button>
       </div>
       <footer className="text-white text-xs my-2 mx-1">
         <div className="grid grid-cols-5 gap-4">
@@ -43,7 +80,6 @@ export default function Home() {
             <p>Environments</p>
           </div>
           <div className="col-span-2 flex flex-col justify-end">
-            <p className={`border w-fit px-1 border-pink-500 transition-all duration-500 ${isBlurred ? 'blur-xl' : ''}`}>Click object to enter scene</p>
             <p>i:</p>
             <p>This project showcases vulnerabilities in different opensource ecosystems. In order to explore simply choose a ecosystem and interact.</p>
             <p>&nbsp;</p>
@@ -56,6 +92,7 @@ export default function Home() {
           </div>
         </div>
       </footer>
+      </div>
     </div>
   );
 }
--- code/next-ecosystems/components/three-scene.tsx
+++ code/next-ecosystems/components/three-scene.tsx
@@ -2,14 +2,23 @@ import React, { useRef } from "react";
 import { Canvas, useFrame } from "@react-three/fiber";
 import * as THREE from 'three';
 import { OrbitControls } from "@react-three/drei";
+import { Ecosystem } from "@/app/page";
 
 interface ThreeSceneProps {
+  ecosystem: Ecosystem;
   onClick: () => void;
   onHover: () => void;
   onNoHover: () => void;
 }
 
-const RotatingMesh: React.FC<ThreeSceneProps> = ({ onClick, onHover, onNoHover }) => {
+interface RotatingMeshProps {
+  color: string;
+  onClick: () => void;
+  onHover: () => void;
+  onNoHover: () => void;
+}
+
+const RotatingMesh: React.FC<RotatingMeshProps> = ({color, onClick, onHover, onNoHover }) => {
   const meshRef = useRef<THREE.Mesh>(null);
 
   useFrame(() => {
@@ -26,22 +35,26 @@ const RotatingMesh: React.FC<ThreeSceneProps> = ({ onClick, onHover, onNoHover }
       onPointerLeave={onNoHover}
     >
       <boxGeometry args={[1, 1, 1]} />
-      <meshStandardMaterial color="orange" />
+      <meshStandardMaterial color={color} />
     </mesh>
   );
 };
 
 export const ThreeScene: React.FC<ThreeSceneProps> = ({
+  ecosystem,
   onClick,
   onHover,
   onNoHover,
 }) => {
+  console.log(ecosystem);
+
+  
   return (
     <div style={{ height: "80vh", width: "100vw" }}>
       <Canvas>
         <ambientLight />
         <pointLight position={[10, 10, 10]} />
-        <RotatingMesh onClick={onClick} onHover={onHover} onNoHover={onNoHover} />
+        <RotatingMesh color={ecosystem.color} onClick={onClick} onHover={onHover} onNoHover={onNoHover} />
         <OrbitControls />
       </Canvas>
     </div>
